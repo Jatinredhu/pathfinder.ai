4eef9083a452b2609604311f461603e2a83d019a
Jatinredhu <jatinredhu2011@gmail.com>
2026-05-04 01:34:31 +0530

    updated api.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-  origin: 'https://your-vercel-app.vercel.app'
+  origin: 'https://pathfinder-66ry9tg68-jatin-s-projects-b5b06dd7.vercel.app'
 }))
 app.use(express.json())
 
